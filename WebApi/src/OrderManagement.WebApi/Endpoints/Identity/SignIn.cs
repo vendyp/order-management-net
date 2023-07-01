12aa12e463e0bec38e73df4213c3b8de0468db00
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-07-01 14:47:22 +0700

    Update logic signin and its unittests

--- WebApi/src/OrderManagement.WebApi/Endpoints/Identity/SignIn.cs
+++ WebApi/src/OrderManagement.WebApi/Endpoints/Identity/SignIn.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OrderManagement.Core.Abstractions;
+using OrderManagement.Core.Entities;
 using OrderManagement.WebApi.Dto;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -6,6 +8,15 @@ namespace OrderManagement.WebApi.Endpoints.Identity;
 
 public class SignIn : BaseEndpoint<SignInRequest, SignInDto>
 {
+    private readonly IUserService _userService;
+    private readonly IDbContext _dbContext;
+
+    public SignIn(IUserService userService, IDbContext dbContext)
+    {
+        _userService = userService;
+        _dbContext = dbContext;
+    }
+
     [HttpPost("sign-in")]
     [SwaggerOperation(
             Summary = "Sign in",
@@ -23,6 +34,23 @@ public class SignIn : BaseEndpoint<SignInRequest, SignInDto>
         if (!validationResult.IsValid)
             return BadRequest(Error.Create("Invalid parameter", validationResult.Construct()));
 
-        throw new NotImplementedException();
+        var user = await _userService.GetUserByUsernameAsync(request.Username!, cancellationToken);
+        if (user is null)
+            return BadRequest(Error.Create("Invalid username or password"));
+
+        if (!_userService.VerifyPassword(request.Password!, user.Salt!, user.HashedPassword!))
+            return BadRequest(Error.Create("Invalid username or password"));
+
+        _dbContext.AttachEntity(user);
+
+        var userToken = new UserToken
+        {
+            RefreshToken = Guid.NewGuid().ToString("N")
+        };
+        user.UserTokens.Add(userToken);
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return new SignInDto();
     }
 }
--- WebApi/tests/OrderManagement.UnitTests/Endpoints/Identity/SignInTests.cs
+++ WebApi/tests/OrderManagement.UnitTests/Endpoints/Identity/SignInTests.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using OrderManagement.Core.Abstractions;
+using OrderManagement.Core.Entities;
 using OrderManagement.WebApi.Endpoints.Identity;
 using Shouldly;
 
@@ -85,7 +88,7 @@ public class SignInTests
     [MemberData(nameof(GetInvalidRequests))]
     public async Task SignIn_Given_InvalidRequest_ShouldReturn_BadRequest(SignInRequest request)
     {
-        var signIn = new SignIn();
+        var signIn = new SignIn(new Mock<IUserService>().Object, new Mock<IDbContext>().Object);
 
         var result = await signIn.HandleAsync(request, CancellationToken.None);
 
@@ -96,5 +99,140 @@ public class SignInTests
         var actual = result.Result as BadRequestObjectResult;
         actual.ShouldNotBeNull();
         actual.Value.ShouldBeOfType<Error>();
+    }
+
+    [Fact]
+    public async Task SignIn_Given_CorrectRequest_With_InvalidEmail_ShouldReturn_BadRequest()
+    {
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(e =>
+                e.GetUserByUsernameAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<CancellationToken>()))
+            .ReturnsAsync((User?)null);
+
+        var signIn = new SignIn(userServiceMock.Object, new Mock<IDbContext>().Object);
+
+        var request = new SignInRequest
+        {
+            Username = "test@test.com",
+            Password = "Qwerty@1234"
+        };
+
+        var result = await signIn.HandleAsync(request, CancellationToken.None);
+
+        // Assert the expected results
+        result.ShouldNotBeNull();
+        result.Result.ShouldNotBeNull();
+        result.Result.ShouldBeOfType(typeof(BadRequestObjectResult));
+        var actual = result.Result as BadRequestObjectResult;
+        actual.ShouldNotBeNull();
+        actual.StatusCode.ShouldBe(400);
+        actual.Value.ShouldBeOfType<Error>();
+        var err = actual.Value as Error;
+        err.ShouldNotBeNull();
+        err.Message.ShouldBe("Invalid username or password");
+    }
+
+    [Fact]
+    public async Task SignIn_Given_CorrectRequest_With_InvalidPassword_ShouldReturn_BadRequest()
+    {
+        var request = new SignInRequest
+        {
+            Username = "test@test.com",
+            Password = "Qwerty@1234"
+        };
+
+        var userServiceMock = new Mock<IUserService>();
+        var dummyUser = new User
+        {
+            Username = "test@test.com",
+            HashedPassword = "bbb",
+            Salt = "aaa"
+        };
+
+        //get username return data
+        userServiceMock.Setup(e =>
+                e.GetUserByUsernameAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new User());
+
+        //when verify password called return false
+        userServiceMock.Setup(e => e.VerifyPassword(request.Password, dummyUser.Salt, dummyUser.HashedPassword))
+            .Returns(false);
+
+        var signIn = new SignIn(userServiceMock.Object, new Mock<IDbContext>().Object);
+
+        var result = await signIn.HandleAsync(request, CancellationToken.None);
+
+        // Assert the expected results
+        result.ShouldNotBeNull();
+        result.Result.ShouldNotBeNull();
+        result.Result.ShouldBeOfType(typeof(BadRequestObjectResult));
+        var actual = result.Result as BadRequestObjectResult;
+        actual.ShouldNotBeNull();
+        actual.StatusCode.ShouldBe(400);
+        actual.Value.ShouldBeOfType<Error>();
+        var err = actual.Value as Error;
+        err.ShouldNotBeNull();
+        err.Message.ShouldBe("Invalid username or password");
+    }
+
+    [Fact]
+    public async Task SignIn_Given_CorrectRequest_Should_Do_As_Expected()
+    {
+        var request = new SignInRequest
+        {
+            Username = "test@test.com",
+            Password = "Qwerty@1234"
+        };
+
+        var userServiceMock = new Mock<IUserService>();
+
+        var dummyUser = new User
+        {
+            Username = request.Username,
+            HashedPassword = "aaa"
+        };
+
+        User? user = null;
+
+        var dbContextMock = new Mock<IDbContext>();
+        dbContextMock.Setup(e => e.AttachEntity(It.IsAny<User>()))
+            .Callback<User>((entity) =>
+            {
+                entity.UserId.ShouldBe(dummyUser.UserId);
+                entity.Username.ShouldBe(dummyUser.Username);
+                entity.HashedPassword.ShouldBe(dummyUser.HashedPassword);
+
+                user = entity;
+            });
+
+        //get username return data
+        userServiceMock.Setup(e =>
+                e.GetUserByUsernameAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<CancellationToken>()))
+            .ReturnsAsync(dummyUser);
+
+        //when verify password called return false
+        userServiceMock.Setup(e => e.VerifyPassword(request.Password, It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(true);
+
+        var signIn = new SignIn(userServiceMock.Object, dbContextMock.Object);
+
+        user.ShouldBeNull();
+
+        _ = await signIn.HandleAsync(request, CancellationToken.None);
+
+        user.ShouldNotBeNull();
+        user.UserTokens.Count.ShouldBeGreaterThan(0);
+        var userToken = user.UserTokens.First();
+        userToken.RefreshToken.ShouldNotBeEmpty();
+        userToken.IsUsed.ShouldBeFalse();
+        userToken.UsedAt.ShouldBeNull();
+
+        dbContextMock.Verify(e => e.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
     }
 }
