226cb980db3e320cbc7c1fa8e899c6e5711c7669
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-07-03 23:15:11 +0700

    Add new xunit project for functionaltests

---
+++ WebApi/tests/OrderManagement.FunctionalTests/RunFunctionalTests.cs
@@ -0,0 +1,12 @@
+using Shouldly;
+
+namespace OrderManagement.FunctionalTests;
+
+public class RunFunctionalTests
+{
+    [Fact]
+    public void Run()
+    {
+        true.ShouldBeTrue();
+    }
+}
